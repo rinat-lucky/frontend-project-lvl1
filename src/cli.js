f05ca5fe0e90136e958735e83efaf020af39fca9
Rinat <64750426+rinat-lucky@users.noreply.github.com>
2022-06-10 17:19:20 +0500

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default const greeting = () => {
+export default greeting = () => {
   console.log('Welcome to the Brain Games!');
   const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}!`);
