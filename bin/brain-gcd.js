f57c9641f48f52fa8a6663b2967ee266f58202fc
Rinat <64750426+rinat-lucky@users.noreply.github.com>
2022-06-10 12:25:52 +0500

    Update brain-gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 
 import { game, getRandomNums, checkAnswer } from '../src/games/gcd.js';
-import { username } from '../src/index.js';
 
 game(
   'Find the greatest common divisor of given numbers.',
   getRandomNums,
   checkAnswer,
-  `Let's try again, ${username}!`,
 );
