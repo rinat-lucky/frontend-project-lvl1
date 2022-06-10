78a174998bf4cd1c6cdd29e5992a71bbb9510a8e
Rinat <64750426+rinat-lucky@users.noreply.github.com>
2022-06-10 12:26:39 +0500

    Update brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 
 import { game, getRandomNum, checkAnswer } from '../src/games/prime.js';
-import { username } from '../src/index.js';
 
 game(
   'Answer "yes" if given number is prime. Otherwise answer "no".',
   getRandomNum,
   checkAnswer,
-  `Let's try again, ${username}!`,
 );
