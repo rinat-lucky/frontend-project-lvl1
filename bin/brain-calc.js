a7c76e851a0af13ae4690eab638298ce59ee7709
Rinat <64750426+rinat-lucky@users.noreply.github.com>
2022-06-10 12:26:19 +0500

    Update brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import { game, getRandomExp, checkAnswer } from '../src/games/calc.js';
-import { username } from '../src/index.js';
 
 game(
   'What is the result of the expression?',
