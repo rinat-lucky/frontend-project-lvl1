134076a5a309d124989c7f51fc2e584b6587388d
rinat-u <rinat1kam@mail.ru>
2022-06-23 08:09:46 +0500

    fix brain-prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,7 +5,7 @@ const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const question = () => getRandomInt(1, 30);
 
 const checkAnswer = (number) => {
-  for (let i = 2; i < number; i += 1) {
+  for (let i = 2; i < number / 2; i += 1) {
     if (number % i === 0 || number === 2) {
       return 'no';
     }
