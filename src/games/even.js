e5f8a89ab6bf92d4e1c53dab1f110c90ac2f7ef7
rinat-u <rinat1kam@mail.ru>
2022-07-02 08:00:52 +0500

    fix typo

--- src/games/even.js
+++ src/games/even.js
@@ -3,9 +3,9 @@ const isEven = (number) => (number % 2 === 0);
 const game = {
   description: 'Answer "yes" if the number is even, otherwise answer "no".',
   generateRound: (getRandomInt) => {
-    const number = getRandomInt();
+    const question = getRandomInt();
     const answer = isEven(number) ? 'yes' : 'no';
-    return [number, answer];
+    return [question, answer];
   },
 };
 
