f2965575554a2d7a0b062fe4a74fd393aa892f53
rinat-u <rinat1kam@mail.ru>
2022-07-04 08:15:27 +0500

    add obj Game in gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,30 +1,31 @@
-import getRandomInt from '../helpers.js';
-
-export default () => {
-  const rules = 'Find the greatest common divisor of given numbers.';
-
-  const genQuestion = () => {
-    const num1 = getRandomInt();
-    const num2 = getRandomInt(15, 25);
-
-    return `${num1} ${num2}`;
-  };
-
-  const question = genQuestion();
-
-  const checkAnswer = () => {
-    const expArr = question.toString().split(' ');
-    const num1 = Number(expArr[0]);
-    const num2 = Number(expArr[1]);
-    let gcd = 1;
-    for (let i = 2; i <= num1; i += 1) {
-      if ((num1 % i === 0) && (num2 % i === 0)) {
-        gcd = i;
+const game = {
+  description: 'Find the greatest common divisor of given numbers.',
+  generateRound: (getRandomInt) => {
+    const genQuestion = () => {
+      const num1 = getRandomInt();
+      const num2 = getRandomInt(15, 25);
+  
+      return `${num1} ${num2}`;
+    };
+  
+    const question = genQuestion();
+  
+    const checkAnswer = () => {
+      const expArr = question.toString().split(' ');
+      const num1 = Number(expArr[0]);
+      const num2 = Number(expArr[1]);
+      let gcd = 1;
+      for (let i = 2; i <= num1; i += 1) {
+        if ((num1 % i === 0) && (num2 % i === 0)) {
+          gcd = i;
+        }
       }
-    }
-    return gcd.toString();
-  };
-
-  const answer = checkAnswer();
-  return [rules, question, answer];
+      return gcd.toString();
+    };
+  
+    const answer = checkAnswer();
+    return [question, answer];
+  },
 };
+
+export default game;
