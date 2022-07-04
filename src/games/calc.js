1670ec5b451c940d6b7c739422f719592c59d086
rinat-u <rinat1kam@mail.ru>
2022-07-04 08:10:58 +0500

    add obj Game in calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,32 +1,33 @@
-export default () => {
-  const description = 'What is the result of the expression?';
+const game = {
+  description: 'What is the result of the expression?',
+  generateRound: (getRandomInt) => {
 
-  const getQuestion = (getRandomInt) => {
-    const num1 = getRandomInt();
-    const num2 = getRandomInt();
-    const operators = ['+', '-', '*'];
-    const lastOperatorIndex = operators.length - 1;
-    const operator = operators[getRandomInt(0, lastOperatorIndex)];
+    const getQuestion = () => {
+      const num1 = getRandomInt();
+      const num2 = getRandomInt();
+      const operators = ['+', '-', '*'];
+      const lastOperatorIndex = operators.length - 1;
+      const operator = operators[getRandomInt(0, lastOperatorIndex)];
+      return `${num1} ${operator} ${num2}`;
+    };
+    const question = getQuestion();
 
-    return `${num1} ${operator} ${num2}`;
-  };
-
-  const question = getQuestion();
-
-  const checkAnswer = (text) => {
-    const [num1, operator, num2] = text.split(' ');
-    switch (operator) {
-      case '+':
-        return (Number(num1) + Number(num2)).toString();
-      case '-':
-        return (Number(num1) - Number(num2)).toString();
-      case '*':
-        return (Number(num1) * Number(num2)).toString();
-      default:
-        return console.log('Произошла ошибка в вычислениях');
-    }
-  };
-  const answer = checkAnswer(question);
-
-  return [description, question, answer];
+    const checkAnswer = (text) => {
+      const [num1, operator, num2] = text.split(' ');
+      switch (operator) {
+        case '+':
+          return (Number(num1) + Number(num2)).toString();
+        case '-':
+          return (Number(num1) - Number(num2)).toString();
+        case '*':
+          return (Number(num1) * Number(num2)).toString();
+        default:
+          return console.log('Произошла ошибка в вычислениях');
+      }
+    };
+    const answer = checkAnswer(question);
+    return [question, answer];
+  },
 };
+
+export default game;
